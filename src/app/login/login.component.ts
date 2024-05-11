5ac9eb3dc5d9028ff64c7bb8eb2f16b8611e3ec2
rogelio-02 <abraham.lopezg@uanl.edu.mx>
2024-05-10 21:37:17 -0500

    Plantilla principal de la app: Tabs, menu y login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/router';
+import { TabsPage } from '../tabs/tabs.page';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
